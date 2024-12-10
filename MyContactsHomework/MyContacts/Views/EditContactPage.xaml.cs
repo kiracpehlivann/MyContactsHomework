8a92b3d0112699996bbc5f7499476ddb0626546e
Kıraç Pehlivan <97739556+kiracpehlivann@users.noreply.github.com>
2024-12-10 15:04:23 +0300

    Edit Contact property is added to MyContact

---
+++ MyContactsHomework/MyContacts/Views/EditContactPage.xaml.cs
@@ -0,0 +1,44 @@
+using MyContacts.Model;
+
+namespace MyContacts.Views;
+
+
+
+[QueryProperty(nameof(id), "id")]
+public partial class EditContactPage : ContentPage
+{
+    ContactsRepository repository = new ContactsRepository();
+
+    public string id { get; set; }
+	public EditContactPage()
+	{
+        
+		InitializeComponent();
+	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var contact = repository.GetContact(Int32.Parse(id));
+        if (contact != null) 
+        {
+            NameEntry.Text = contact.NameSurname;
+            PhoneEntry.Text = contact.PhoneNumber;
+            EmailEntry.Text = contact.Email;
+            selectedContact.Text = id + " " + contact.NameSurname;
+        }
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        var updatedContact = new ContactInfo
+        {
+            Id = Int32.Parse(id),
+            NameSurname = NameEntry.Text,
+            PhoneNumber = PhoneEntry.Text,
+            Email = EmailEntry.Text
+        }
+        repository.UpdateContact(updatedContact);
+        Shell.Current.GoToAsync("..");
+ 
+    }
+}
