cb5e0a9089ed953125a29ce36b56e3739173a16b
Kıraç Pehlivan <97739556+kiracpehlivann@users.noreply.github.com>
2024-12-17 13:42:28 +0300

    Data base feature added

---
+++ Model/ContactInfo.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContacts.Model
+{
+    public class ContactInfo
+    {
+        [AutoIncrement]
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
